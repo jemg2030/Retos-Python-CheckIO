4fc1315078b13968a5094d3986ab04f1e77d11a2
jemg <jmeneses@agrosan.com.co>
2023-03-26 23:46:28 -0500

    Inicio Y Solucion del problema Sort Sorted Groups

---
+++ DROPBOX/SortSortedGroups.py
@@ -0,0 +1,86 @@
+'''
+You are given a list of integers. Your goal is to find all sorted groups (group of numbers with distinct
+sorting order or single value) inside the list, sort input list by these groups and return this sorted
+list with groups unpacked.
+
+For example, [5, 1, 5, 0, 5] --> [[5, 1], [5, 0], [5]] --> [[5], [5, 0], [5, 1]] --> [5, 5, 0, 5, 1].
+
+If adjacent numbers are equal, consider sorting order remains the same. If a group starts with equal numbers,
+seek the different number further to determine or check sorting order, or end of the list. You should find out
+the sorting order for each group, not once for the whole list. If the first group has the descending order, it
+doesn’t mean, you should build all you groups as descending. You should be greedy from the left - take as much
+numbers as possible to build and determine the sequence with distinct order. Good luck!
+
+Input: List of integers.
+
+Output: List of integers.
+
+Examples:
+assert sorted_groups([]) == []
+assert sorted_groups([5]) == [5]
+assert sorted_groups([5, 1, 5, 0, 5]) == [5, 5, 0, 5, 1]
+assert sorted_groups([5, 5, 1]) == [5, 5, 1]
+
+----------
+----------
+
+Se le da una lista de números enteros. Su objetivo es encontrar todos los grupos ordenados (grupo de
+números con distinto orden de clasificación o valor único) dentro de la lista, ordenar la lista de
+entrada por estos grupos y devolver esta lista ordenada con los grupos desempaquetados.
+
+Por ejemplo, [5, 1, 5, 0, 5] --> [[5, 1], [5, 0], [5]] --> [[5], [5, 0], [5, 1]] --> [5, 5, 0, 5, 1].
+
+Si los números adyacentes son iguales, considere el orden de clasificación sigue siendo el mismo. Si un
+grupo comienza con números iguales, busque el número diferente más adelante para determinar o registrar
+el orden de clasificación, o el final de la lista. Debe averiguar el orden de clasificación para cada grupo,
+no una vez para toda la lista. Si el primer grupo es descendente, no significa que todos los grupos deban ser
+descendentes. Usted debe ser codicioso de la izquierda - tomar tantos números como sea posible para construir
+y determinar la secuencia con orden distinto. Buena suerte.
+
+Entrada: Lista de enteros.
+
+Salida: Lista de enteros.
+
+Ejemplo:
+assert grupos_ordenados([]) == []
+assert grupos_ordenados([5]) == [5]
+assert grupos_ordenados([5, 1, 5, 0, 5]) == [5, 5, 0, 5, 1]
+assert grupos_ordenados([5, 5, 1]) == [5, 5, 1]
+'''
+
+
+def sorted_groups(items: list[int]) -> list[int]:
+    # your code here
+    result = []
+    i = 0
+    n = len(items)
+
+    while i < n:
+        j = i + 1
+
+        # find the end of the sorted group
+        while j < n and items[j] >= items[j - 1]:
+            j += 1
+
+        # sort the sorted group according to its order
+        if j - i > 1:
+            if items[i] < items[j - 1]:
+                result.append(sorted(items[i:j]))
+            else:
+                result.append(sorted(items[i:j], reverse=True))
+        else:
+            result.append([items[i]])
+
+        i = j
+
+    # flatten the result list
+    return [x for group in result for x in group]
+
+
+# These "asserts" are used for self-checking
+assert sorted_groups([]) == []
+assert sorted_groups([5]) == [5]
+assert sorted_groups([5, 1, 5, 0, 5]) == [5, 5, 0, 5, 1]
+assert sorted_groups([5, 5, 1]) == [5, 5, 1]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
