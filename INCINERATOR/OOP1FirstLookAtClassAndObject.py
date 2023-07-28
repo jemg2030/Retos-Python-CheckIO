c49a07a7523de646c2eda26baac2f10c67b15c0a
jemg <jmeneses@agrosan.com.co>
2023-07-28 11:16:34 -0500

    Solución del problema OOP 1: First Look At Class And Object

---
+++ INCINERATOR/OOP1FirstLookAtClassAndObject.py
@@ -0,0 +1,35 @@
+"""
+The main OOP idea is to describe in programming language any object (incl. ones from the
+real life) by defyining its its characteristics, behavior etc.
+
+This series of tasks will help you learn the basics of OOP in Python, such as creating classes,
+attributes, and methods, inheritance, method overrides, using super(), and polymorphism. If you
+need additional help, you may use this documentation or another one.
+
+1.1. Create a Car class that contains no methods or attributes.
+
+1.2. Create an object my_car from the Car class.
+
+----------
+----------
+
+La idea principal de la programación orientada a objetos es describir en lenguaje de
+programación cualquier objeto (incluidos los de la vida real) definiendo sus características,
+comportamiento, etc.
+
+Esta serie de tareas te ayudarán a aprender los fundamentos de la programación orientada a
+objetos en Python, como la creación de clases, atributos y métodos, herencia, anulación de métodos,
+uso de super() y polimorfismo. Si necesitas ayuda adicional, puedes utilizar esta documentación u otra.
+
+1.1. Crea una clase Coche que no contenga métodos ni atributos.
+
+1.2. Crea un objeto mi_coche a partir de la clase Coche.
+"""
+
+
+# show me some OOP magic here
+class Car:
+    pass
+
+
+my_car = Car()
